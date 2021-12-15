3d0d833a5194b59b104dc17b161dd1790d81f238
Metehan-Altuntekin <metehanaltuntekin@hotmail.com>
2021-12-15 22:06:23 +0300

    previous function needed for creating new animation due to distance change was too heavy. changed it to a more efficient one

--- scripts/squares.js
+++ scripts/squares.js
@@ -10,15 +10,19 @@ document.getElementById("density-input").addEventListener("input", function () {
   cycle = setInterval(CreateSquare, density());
 });
 
-var csCounter = 0;
-function CreateSquare() {
-  const zone = document.getElementById("animation-zone");
-  const square = document.createElement("div");
-
-  //This part of code is slowing down a lot. Works but need an efficient one.
-  const animation = document.createElement("style");
-  animation.append(
-    '@keyframes Animator' + csCounter + ' {' +
+var distanceInputCounter = 0;   //This variable is created to use for giving unique id for different css animations and their containers (style)
+const animationDurations = [];  //This array is created for getting the duration value of previous animations, after new animation. This way, remove function know when to delete the animation
+NewAnimationCSS();
+document.getElementById("distance-input").addEventListener("input", NewAnimationCSS);  //Need to create new animation css @keyframes for new distance
+function NewAnimationCSS() {
+  distanceInputCounter++;   //How many times the distance value is changed
+  if (distanceInputCounter > 1000) {    //Don't want to increase that value too much
+    distanceInputCounter == 0;
+  }
+  let animationCSS = document.createElement("style");
+  animationCSS.id = "AnimationCSS" + distanceInputCounter;
+  animationCSS.append(
+    '@keyframes Animator' + distanceInputCounter + ' {' +
     '0% {' +
     'opacity: 0;' +
     'transform: translateY(0px) rotate(0deg) scale(0);' +
@@ -38,9 +42,19 @@ function CreateSquare() {
 
     '}' +
     '}'
-
   )
-  square.appendChild(animation);
+  document.head.appendChild(animationCSS);
+  var previousDistanceInputCounter = distanceInputCounter - 1     //This variable used for determining which animation box (style) to delete
+  setTimeout(() => {
+    let a = document.getElementById("AnimationCSS" + previousDistanceInputCounter);
+    a.remove();
+  }, animationDurations[previousDistanceInputCounter] * 1000);
+
+}
+
+function CreateSquare() {
+  const zone = document.getElementById("animation-zone");
+  const square = document.createElement("div");
 
   square.style.backgroundColor = RandomHex(6);
   square.style.top = RandomPercentage();
@@ -49,10 +63,10 @@ function CreateSquare() {
   square.style.width = size();
   square.style.height = size();
 
-  square.style.animation = "Animator" + csCounter + " " + duration() + "s linear infinite";
+  square.style.animation = "Animator" + distanceInputCounter + " " + duration() + "s linear infinite";
   zone.appendChild(square);
 
-  csCounter++;
+  animationDurations[distanceInputCounter] = duration();
   setTimeout(() => {
     square.remove();
   }, duration() * 1000)
